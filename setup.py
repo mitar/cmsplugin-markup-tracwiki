bd11a2696dcdc0ad544fdefd9d4366e2ffadd0d7
Mitar <mitar.git@tnode.com>
2011-07-28 23:49:11 +0200

    Improving setup and readme files.

--- setup.py
+++ setup.py
@@ -1,22 +1,33 @@
-from setuptools import setup
+import os
+
+from setuptools import setup, find_packages
 
 VERSION = '0.1'
-PACKAGE = 'cmsplugin_markup_tracwiki'
 
 setup(
     name = 'cmsplugin-markup-tracwiki',
     version = VERSION,
     description = 'Trac wiki engine integration with Django CMS as a plugin for cmsplugin-markup.',
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read(),
     author = 'Mitar',
-    author_email = 'mitar.markup@tnode.com',
-    url = 'http://mitar.tnode.com/',
+    author_email = 'mitar.django@tnode.com',
+    url = 'https://bitbucket.org/mitar/cmsplugin-markup-tracwiki',
     license = 'GPLv3',
-    packages = [PACKAGE],
+    packages = find_packages(),
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
     include_package_data = True,
+    zip_safe = False,
     install_requires = [
         'Django>=1.2',
         'trac>=0.12',
         'cmsplugin-markup>=0.1',
     ],
-    zip_safe = False
 )
